82ba2c58a7585e87c1183adc54194e918b921202
purecloud75 <purecloud75@gmail.com>
2022-07-29 23:32:34 +0900

    220729

---
+++ src/main/java/com/shop/Controller/MainController.java
@@ -0,0 +1,14 @@
+package com.shop.Controller;
+// 회원가입 후 메인 페이지로 갈 수 있도록 한다.
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String main() {
+        return "main";  // 메인 페이지는 추후 등록된 상품의 목록을 보여주는 곳이 될거다.
+    }
+}
